8b5876228dacadbaa7ca740d167da4cbbcb077b0
Chris Bynum <cbynum@gmail.com>
2012-12-03 22:36:30 -0500

    Node creation with anormcypher added.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,18 +2,40 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 import models.Contact
 
 object Application extends Controller {
   
+  val contactsForm: Form[Contact] = Form(
+  	mapping(
+  	"fname" -> text,
+  	"lname" -> text,
+    "email" -> text,
+  	"title" -> text,
+  	"location" -> text
+  	)(Contact.apply)(Contact.unapply)  
+  )
+
   def index = Action {
     Redirect(routes.Application.contacts)
   }
 
   def contacts = Action {
-  	Ok(views.html.index(Contact.all()))
+  	Ok(views.html.index(Contact.all(), contactsForm))
   }
 
-  def deleteContact (id:Int) = TODO
+  def newContact = Action { implicit request =>
+  	contactsForm.bindFromRequest.fold(
+  		errors => BadRequest(views.html.index(Contact.all(), errors)),
+  		contact => {
+        Contact.create(contact.fname, contact.lname, contact.email, contact.title, contact.location)
+  			Redirect(routes.Application.contacts)
+  		}
+  	)
+  }
+
+  def deleteContact (fname:String) = TODO
   
 }
--- app/models/Contact.scala
+++ app/models/Contact.scala
@@ -1,14 +1,32 @@
 package models
 
+import org.anormcypher._
 import scala.collection.mutable.ListBuffer
+import play.Logger
 
-case class Contact(id:Int, fname:String, lname:String, title:String, location:String)
+case class Contact(fname:String, lname:String, email:String, title:String, location:String)
 
 object Contact {
 
 	def all(): List[Contact] = {
 		var contacts = new ListBuffer[Contact]
 		contacts.toList
+	}
+
+	def create(fname:String, lname:String, email:String, title:String, location:String) {
+		Logger.info("Creating Contact...")
+		Logger.info("First Name: " + fname)
+		Logger.info("Last Name: " + lname)
+		Logger.info("Email: " + email)
+		Logger.info("Title: " + title)
+		Logger.info("Location: " + location)
+
+		val result = Cypher("""
+			create (contact {firstName:{fnameParam}, lastName:{lnameParam}, email:{emailParam}, title:{titleParam}, location:{locationParam} });
+			""").on("fnameParam" -> fname, "lnameParam" -> lname, "emailParam" -> email,
+					"titleParam" -> title, "locationParam" -> location).execute()
+
+		Logger.info("Created Contact: " + result)
 	}
 
 }
